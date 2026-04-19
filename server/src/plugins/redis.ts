53f270f130e25cd63568d8df1480cd3033aa98e7
natadecua <nathanael_cua@dlsu.edu.ph>
2026-04-19 16:17:53 +0800

    feat(server): Prisma, Redis, and auth plugins

--- server/src/plugins/redis.ts
+++ server/src/plugins/redis.ts
@@ -1,3 +1,15 @@
 import fp from 'fastify-plugin'
+import Redis from 'ioredis'
 
-export const redisPlugin = fp(async () => {})
+declare module 'fastify' {
+  interface FastifyInstance {
+    redis: Redis
+  }
+}
+
+export const redisPlugin = fp(async (fastify) => {
+  const redis = new Redis(process.env['REDIS_URL']!, { lazyConnect: true })
+  await redis.connect()
+  fastify.decorate('redis', redis)
+  fastify.addHook('onClose', async () => { await redis.quit() })
+})
